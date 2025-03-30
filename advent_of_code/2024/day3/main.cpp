2b37888bd7308a72373f2fa7e4207997b4bb8564
AloizioMacedo <atsam2@hotmail.com>
2025-03-30 15:36:47 -0300

    Solve P2 of aoc2024/day3

--- advent_of_code/2024/day3/main.cpp
+++ advent_of_code/2024/day3/main.cpp
@@ -69,6 +69,75 @@ int solve_p1(const std::string &contents) {
   return total;
 }
 
+enum class OperationTag { Do, Dont, Mul };
+
+union Value {
+  Mul mul;
+  void *none;
+};
+
+class Operation {
+public:
+  OperationTag tag;
+  Value value;
+};
+
+// Seems that regex_search does not work with `string_view`.
+// Why is `string` really needed? Why is a string_view not enough??
+std::vector<Operation> parse_ops(const std::string &contents) {
+  std::regex pat(R"(mul\(\d+,\d+\)|do\(\)|don't\(\))");
+
+  std::sregex_iterator begin(contents.begin(), contents.end(), pat);
+  std::sregex_iterator end;
+
+  std::vector<Operation> ops;
+  for (auto it = begin; it != end; it++) {
+    auto s = it->str();
+
+    if (s.substr(0, 3) == "don") {
+      auto op = (Operation){OperationTag::Dont, (Value){.none = nullptr}};
+
+      ops.push_back(op);
+    } else if (s.substr(0, 2) == "do") {
+      auto op = (Operation){OperationTag::Do, (Value){.none = nullptr}};
+
+      ops.push_back(op);
+    } else {
+      auto block = MulBlock(it->str());
+      auto mul = block.to_mul();
+      auto op = (Operation){OperationTag::Mul, (Value){.mul = mul}};
+
+      ops.push_back(op);
+    }
+  }
+
+  return ops;
+}
+
+int solve_p2(const std::string &contents) {
+  std::vector<Operation> ops = parse_ops(contents);
+
+  int total = 0;
+  auto should_do = true;
+
+  for (const auto &op : ops) {
+    switch (op.tag) {
+    case OperationTag::Do:
+      should_do = true;
+      break;
+    case OperationTag::Dont:
+      should_do = false;
+      break;
+    case OperationTag::Mul:
+      if (should_do) {
+        total += op.value.mul.apply();
+      }
+    }
+  }
+
+  return total;
+}
+
 int main() {
   std::ifstream f;
   f.open("input.txt");
@@ -80,4 +149,7 @@ int main() {
 
   auto result1 = solve_p1(contents);
   std::cout << "P1: " << result1 << "\n";
+
+  auto result2 = solve_p2(contents);
+  std::cout << "P2: " << result2 << "\n";
 }
